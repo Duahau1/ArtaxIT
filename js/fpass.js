00042bf5f51f4560eeacf280a29354e7712a6492
juanluisja1 <jlja2@ensign.edu>
2021-02-24 11:58:14 -0700

    js files for forget and reset pass

---
+++ js/fpass.js
@@ -0,0 +1,9 @@
+
+// endpoint https://mcval.herokuapp.com/user/resetpassword
+// { required to reset password
+//     "email": "myemail@gmail.com",
+//     "username": "secured"
+//    }
+
+// test with other than test8 account
+
